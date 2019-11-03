4556eeccbc6c7636036dc36f351d335c72a27acc
ellynhan <38302837+ellynhan@users.noreply.github.com>
2019-11-03 12:11:07 -0800

    data from mysql to php file
    data downloading and database connecting

--- basicDataUse.php
+++ basicDataUse.php
@@ -2,20 +2,22 @@
 <html>
 <body>
 <?php
-$conn = mysqli_connect("hostname", "username", "password", "databasename");
-$result = $conn->query("SELECT NAME, STUDENTID FROM Student");
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) { //or fetch_array()
-        echo "<br> id: " . $row["STUDENTID"] . " - Name: " . $row["NAME"] . "<br>";
+    $conn = mysqli_connect("hostname", "username", "password", "databasename");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-} else {
-    echo "0 results";
-}
+    $result = $conn->query("SELECT NAME, STUDENTID FROM Student");
 
-$conn->close();
+    if ($result->num_rows > 0) {
+     // output data of each row
+        while ($row = $result->fetch_assoc()) { //or fetch_array()
+            echo "<br> id: " . $row["STUDENTID"] . " - Name: " . $row["NAME"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
 
+    $conn->close();
 ?>
 </body>
 </html>
